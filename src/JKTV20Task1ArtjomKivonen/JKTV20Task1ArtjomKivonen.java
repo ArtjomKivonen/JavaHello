e15b49f810a40152283a771230a39a0922cddbd3
pupil <pupil@ivkhk.ee>
2021-09-30 13:26:31 +0300

    TASK1

---
+++ src/JKTV20Task1ArtjomKivonen/JKTV20Task1ArtjomKivonen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JKTV20Task1ArtjomKivonen;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV20Task1ArtjomKivonen {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Java Hello!");
+
+    }
+    
+}
